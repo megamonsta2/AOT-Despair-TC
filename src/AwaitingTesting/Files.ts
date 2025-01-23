02b7f4c1cef1a9bff0835289310bac10d22b48e2
megamonsta2 <sam9kermanshah@gmail.com>
2025-01-23 12:15:49 +0000

    added CreateDir() in AT Files module

--- src/AwaitingTesting/Files.ts
+++ src/AwaitingTesting/Files.ts
@@ -1,6 +1,10 @@
-import { readdir, unlink, writeFile } from "fs/promises";
+import { access, mkdir, readdir, unlink, writeFile } from "fs/promises";
 import { join } from "path";
 
+import {
+  INPUT_FOLDER as MAIN_INPUT_FOLDER,
+  PARSED_FOLDER as MAIN_PARSED_FOLDER,
+} from "../config/Paths.js";
 import {
   INPUT_FOLDER,
   INPUT_FILES,
@@ -9,6 +13,10 @@ import {
 } from "../config/AwaitingTesting/Paths.js";
 
 export default async function ResetFiles() {
+  await CreateDir(MAIN_INPUT_FOLDER);
+  await CreateDir(MAIN_PARSED_FOLDER);
+  await CreateDir(INPUT_FOLDER);
+  await CreateDir(PARSED_FOLDER);
   // Delete existing files
   await Promise.all([RemoveFiles(INPUT_FOLDER), RemoveFiles(PARSED_FOLDER)]);
 
@@ -19,6 +27,14 @@ export default async function ResetFiles() {
   ]);
 
   console.log("Reset files!");
+}
+
+async function CreateDir(path: string) {
+  try {
+    await access(path);
+  } catch {
+    await mkdir(path);
+  }
 }
 
 async function RemoveFiles(path: string) {
